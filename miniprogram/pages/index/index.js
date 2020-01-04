25d17274830a903900380c979201aa9a2e20f54b
[phggg] <[807760472@qq.com]>
2020-01-04 08:18:23 +0800

    首页轮播

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -5,7 +5,20 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    swiperImgUrls: [{
+      url: 'http://fdfs.xmcdn.com/group70/M07/C8/3F/wKgO2F4NtbPjar-NAAG6Bzqt_Ic667.jpg',
+    },
+    {
+      url: 'http://fdfs.xmcdn.com/group72/M02/87/C3/wKgO0F4NtRDgev1YAAGEx1qy1Vs507.jpg',
+    },
+    {
+      url: 'http://fdfs.xmcdn.com/group72/M01/87/F0/wKgO0F4NtrHgLjpMAAGmCSkT8o0701.jpg',
+    },
+    {
+      url: 'http://fdfs.xmcdn.com/group68/M0B/E8/3B/wKgMeF3FYITCmbz6AAGT70SHlfo381.jpg',
+    }
+    ],
+    playlist: [],
   },
 
   /**
