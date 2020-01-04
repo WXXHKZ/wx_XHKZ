3e92451adfb054a778c8237d528efb2b04fda59f
chenxuejing <18950799178@163.com>
2020-01-04 22:34:39 +0800

    2020-1-4 pm

--- miniprogram/pages/search/search.js
+++ miniprogram/pages/search/search.js
@@ -5,7 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+     listdata:[],
+     historydata:["德云社"],
+     value:'',
+     searchdata:[]
   },
 
   /**
@@ -19,7 +22,15 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    wx.request({
+      url: 'https://search.ximalaya.com/hotWordBillboard/list/2.0?categoryId=-1&size=8',
+      method: 'get',
+      success:(res)=>{
+        this.setData({
+          listdata: res.data.hotWordResultList
+        })
+      }
+    })
   },
 
   /**
@@ -62,5 +73,58 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  handledelate:function(){
+    this.setData({
+      historydata: ''
+    })
+  },
+
+  handleinput:function(e){
+    this.setData({
+      value: e.detail.value
+    })
+  },
+
+  handleclear:function(e){
+    this.setData({
+      value:''
+    })
+  },
+
+  handlesearch:function(e){
+      wx.request({
+        url: `https://m.ximalaya.com/m-revision/page/search?kw=${this.data.value}&core=all&page=1&rows=20`, 
+        method: 'get',
+        success: (res) => {
+          this.setData({
+            searchdata: res.data.data.albumViews.albums
+          })
+          console.log(this.data.searchdata)
+        }
+      })
+    let sign=false
+    for (let i = 0; i < this.data.historydata.length;i++){
+      if (this.data.value === this.data.historydata[i]){
+        sign = true
+      }
+    }
+    if (!sign){
+      this.setData({
+        historydata: this.data.historydata.concat(this.data.value)
+      })
+    }
+    
+  },
+//搜索页面主播模块
+  //https://m.ximalaya.com/m-revision/page/search?kw=${this.data.value}&core=user&page=1&rows=20&condition=voice
+//搜索页面声音模块
+  //https://m.ximalaya.com/m-revision/page/search?kw=${this.data.value}&core=track&page=1&rows=20
+
+  handletosearch:function(e){
+    this.setData({
+      value: e.currentTarget.dataset.content
+    })
   }
 })
