3adbafe7bbbbae259e467fe0eab6d5411e2996f6
chenxuejing <18950799178@163.com>
2020-01-05 19:18:12 +0800

    2020-1-5

--- miniprogram/pages/search/search.js
+++ miniprogram/pages/search/search.js
@@ -8,14 +8,7 @@ Page({
      listdata:[],
      historydata:["德云社"],
      value:'',
-     searchdata:[]
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+     searchdata:''
   },
 
   /**
@@ -33,48 +26,7 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-
+  
   handledelate:function(){
     this.setData({
       historydata: ''
@@ -91,6 +43,9 @@ Page({
     this.setData({
       value:''
     })
+    this.setData({
+      searchdata: ''
+    })
   },
 
   handlesearch:function(e){
@@ -99,9 +54,9 @@ Page({
         method: 'get',
         success: (res) => {
           this.setData({
-            searchdata: res.data.data.albumViews.albums
+            searchdata: res.data.data
           })
-          console.log(this.data.searchdata)
+          console.log(res.data.data)
         }
       })
     let sign=false
@@ -127,4 +82,5 @@ Page({
       value: e.currentTarget.dataset.content
     })
   }
+  
 })
