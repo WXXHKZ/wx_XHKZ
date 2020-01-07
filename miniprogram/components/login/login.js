138dc946225e72e4845ec5044495bd80ccf21c3f
[phggg] <[807760472@qq.com]>
2020-01-07 13:58:11 +0800

    发现页面提交

---
+++ miniprogram/components/login/login.js
@@ -0,0 +1,37 @@
+// components/login/login.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    modalShow: Boolean
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onGotUserInfo(event) {
+      console.log(event)
+      const userInfo = event.detail.userInfo
+      console.log(1)
+      // 允许授权
+      if (userInfo) {
+        this.setData({
+          modalShow: false
+        })
+        console.log(userInfo)
+        this.triggerEvent('loginSucc', userInfo)
+      } else {
+        this.triggerEvent('loginFail')
+      }
+    }
+  }
+})
